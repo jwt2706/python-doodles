7f87e279be24d5ea11de7722278893add0db8f41
jwt2706 <jwt2home@gmail.com>
2022-11-01 11:01:19 -0400

    config

--- Francais/Calculs/compteARebour.py
+++ Francais/Calculs/compteARebour.py
@@ -1,12 +1,4 @@
-#compteur = 10
-#while(compteur >= 0):
-    #print(compteur)
-    #compteur = compteur + 1
-
-#code original^
-
-
-compteur = 10
+compteur = int(input("Entrez un nombre: "))
 while(compteur >= 1): #le nombre ici doit etre 1 parce qu'on veut seulment afficher les nombres entiers de 10 a 1, et non 0
     print(compteur)
     compteur = compteur - 1 #le compteur doit dessendre, pas remonter (donc -)
--- to-comment/l4/lab4ex2(for).py
+++
@@ -1,4 +0,0 @@
-n = int(input("Entrez une valeur pour 'n': ")) #demande une valeur pour 'n'
-
-for i in range(1, n + 1): #imprime toutes les nombres de 1 justqu'a 'n'
-    print(i)
